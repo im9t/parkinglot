1ec6a8c62aa91a493c72aae0c2b67ece8e2aef34
sheng.cai <lesincs923@gmail.com>
2022-07-30 16:53:37 +0800

    finish story 2: GraduateParkingBoy

--- src/main/java/org/oobootcamp/ParkingLot/ParkingBoy.java
+++ src/main/java/org/oobootcamp/ParkingLot/ParkingBoy.java
@@ -8,14 +8,29 @@ import org.oobootcamp.ParkingLot.Model.Ticket;
 
 public class ParkingBoy {
 
-    private ArrayList<ParkingLot> parkingLots;
+    private final ArrayList<ParkingLot> parkingLots;
+
     public ParkingBoy(ArrayList<ParkingLot> parkingLots) {
-        this.parkingLots = new ArrayList<ParkingLot>();
-        this.parkingLots.addAll(parkingLots);
+        this.parkingLots = new ArrayList<>(parkingLots);
     }
 
     public Result<Ticket> park(Car car) {
-        
+        for (ParkingLot parkingLot : parkingLots) {
+            var parkingResult = parkingLot.park(car);
+            if (parkingResult.isSucceed) {
+                return parkingResult;
+            }
+        }
+        return new Result<>(false, "停车位已满", null);
     }
 
+    public Result<Car> pickUp(Ticket ticket) {
+        for (ParkingLot parkingLot : parkingLots) {
+            var pickUpResult = parkingLot.pickUp(ticket);
+            if (pickUpResult.isSucceed) {
+                return  pickUpResult;
+            }
+        }
+        return new Result<>(false, "Ticket 无效", null);
+    }
 }
--- src/test/java/org/oobootcamp/ParkingLot/ParkingBoyTest.java
+++ src/test/java/org/oobootcamp/ParkingLot/ParkingBoyTest.java
@@ -1,6 +1,9 @@
 package org.oobootcamp.ParkingLot;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -15,10 +18,8 @@ import org.oobootcamp.ParkingLot.Model.Result;
 import org.oobootcamp.ParkingLot.Model.Ticket;
 
 public class ParkingBoyTest {
-    //Given 管理A和B两个停车场的小弟, A停车场已停满，B停车场有50个可用车位，When 停车, Then 停车成功，并获取到 Ticket
     @Test
-    void should_parking_succeed_and_get_ticked_when_parking_car_by_parking_boy_given_parking_boy_has_available_parking_lot()
-    {
+    void should_parking_succeed_and_get_ticked_when_parking_car_given_parking_boy_has_available_parking_lot() {
         ArrayList<ParkingLot> parkingLots = new ArrayList<>();
         parkingLots.add(new ParkingLot(0));
         parkingLots.add(new ParkingLot(2));
@@ -29,5 +30,95 @@ public class ParkingBoyTest {
 
         assertTrue(parkingResult.isSucceed);
         assertNotNull(parkingResult.value);
+    }
+
+    @Test
+    void should_parking_failed_and_show_error_message_when_parking_car_given_parking_boy_has_no_available_parking_lot() {
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot(0));
+        parkingLots.add(new ParkingLot(0));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        Car car = new Car();
+
+        Result<Ticket> parkingResult = parkingBoy.park(car);
+
+        assertFalse(parkingResult.isSucceed);
+        assertNull(parkingResult.value);
+        assertEquals("停车位已满", parkingResult.errorMessage);
+    }
+
+    @Test
+    void should_parking_by_order_when_parking_car_given_parking_boy_has_available_parking_lot() {
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLotA = new ParkingLot(1);
+        parkingLots.add(parkingLotA);
+        ParkingLot parkingLotB = new ParkingLot(1);
+        parkingLots.add(parkingLotB);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        Car car = new Car();
+
+        Result<Ticket> parkingResult = parkingBoy.park(car);
+        assertTrue(parkingResult.isSucceed);
+
+        // this indicates that the card is parked in A parking lot
+        Result<Ticket> parkingResultForA = parkingLotA.park(new Car());
+        assertFalse(parkingResultForA.isSucceed);
+    }
+
+    @Test
+    void should_pick_up_succeed_when_pick_up_given_valid_ticket() {
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLotA = new ParkingLot(1);
+        parkingLots.add(parkingLotA);
+        ParkingLot parkingLotB = new ParkingLot(1);
+        parkingLots.add(parkingLotB);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        Car car = new Car();
+        Result<Ticket> parkingResult = parkingBoy.park(car);
+        assertTrue(parkingResult.isSucceed);
+
+        Result<Car> pickUpResult = parkingBoy.pickUp(parkingResult.value);
+
+        assertTrue(pickUpResult.isSucceed);
+        assertNotNull(pickUpResult.value);
+        assertEquals(car.number, pickUpResult.value.number);
+    }
+
+    @Test
+    void should_pick_up_failed_when_pick_up_given_ticket_belongs_to_other_parking_boy() {
+        ParkingBoy parkingBoyOne = new ParkingBoy(new ArrayList<>() {
+            {
+                add(new ParkingLot(1));
+            }
+        });
+        ParkingBoy parkingBoyTwo = new ParkingBoy(new ArrayList<>() {
+            {
+                add(new ParkingLot(1));
+            }
+        });
+        Result<Ticket> parkResultFromParkingBoyOne = parkingBoyOne.park(new Car());
+
+        Result<Car> pickUpResult = parkingBoyTwo.pickUp(parkResultFromParkingBoyOne.value);
+
+        assertFalse(pickUpResult.isSucceed);
+        assertEquals("Ticket 无效", pickUpResult.errorMessage);
+    }
+
+    @Test
+    void should_pick_up_failed_when_pick_up_given_ticket_has_already_used() {
+        ParkingBoy parkingBoy = new ParkingBoy(new ArrayList<>() {
+            {
+                add(new ParkingLot(1));
+            }
+        });
+        Result<Ticket> parkingResult = parkingBoy.park(new Car());
+        assertTrue(parkingResult.isSucceed);
+        Result<Car> pickUpResult = parkingBoy.pickUp(parkingResult.value);
+        assertTrue(pickUpResult.isSucceed);
+
+        Result<Car> secondPickUpResult = parkingBoy.pickUp(parkingResult.value);
+
+        assertFalse(secondPickUpResult.isSucceed);
+        assertEquals("Ticket 无效", secondPickUpResult.errorMessage);
     }
 }
